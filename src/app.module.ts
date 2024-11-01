6c34cd278cce734597e243e1823a38cbe63310b3
Facu bobarini <facubobarini@gmail.com>
2024-11-01 01:32:05 -0300

    add MongooseModule w/ config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { ReadModuleModule } from './read-module/read-module.module';
 import { UpdateMolduleModule } from './update-moldule/update-moldule.module';
 import { DeleteModuleModule } from './delete-module/delete-module.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [ ConfigModule.forRoot({
     envFilePath: '.env',
     isGlobal : true
   }),
+  MongooseModule.forRoot(process.env.DB_ACCES),
   CreateModuleModule, 
   ReadModuleModule, 
   UpdateMolduleModule, 
