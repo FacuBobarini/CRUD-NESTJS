534dcb861183486a4d0b69f35b2785a7c7cb662c
Facu bobarini <facubobarini@gmail.com>
2024-11-01 01:17:23 -0300

    add ConfigModule to imports w/ forRoot function and env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { CreateModuleModule } from './create-module/create-module.module';
 import { ReadModuleModule } from './read-module/read-module.module';
 import { UpdateMolduleModule } from './update-moldule/update-moldule.module';
 import { DeleteModuleModule } from './delete-module/delete-module.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CreateModuleModule, ReadModuleModule, UpdateMolduleModule, DeleteModuleModule ],
-  controllers: [],
-  providers: [],
+  imports: [ ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal : true
+  }),
+  CreateModuleModule, 
+  ReadModuleModule, 
+  UpdateMolduleModule, 
+  DeleteModuleModule
+]
 })
 export class AppModule {}
