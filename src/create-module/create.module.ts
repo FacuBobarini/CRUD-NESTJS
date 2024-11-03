066e13da708a580f15938fa25c84bd20b8e995e1
Facu bobarini <facubobarini@gmail.com>
2024-11-03 17:03:25 -0300

    change Create module class and file

---
+++ src/create-module/create.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Create, CreateSchema } from 'src/schemas/Create.schema';
+
+@Module({})
+export class CreateModule {}
