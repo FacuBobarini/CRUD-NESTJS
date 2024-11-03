e3d72579faecc2d3935234973908e785a7658fc4
Facu bobarini <facubobarini@gmail.com>
2024-11-03 20:21:59 -0300

    add addNewPerson function to PersonService Class

--- src/person-module/person.service.ts
+++ src/person-module/person.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Person } from "src/schemas/Person.schema";
+import { CreatePersonDTO } from "./dto/person.dto";
+import { createPerson } from "./interface/person.interface";
 
 
 @Injectable()
-export class AppointmentService {
+export class PersonService {
   constructor (@InjectModel(Person.name) private personModel: Model<Person>){}
+
+  async addNewPerson(person:CreatePersonDTO):Promise<createPerson>{
+
+    return new this.personModel(person).save()
+}
 
     
 }
