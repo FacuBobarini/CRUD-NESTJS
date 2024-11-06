a118f95389ab11b8c4322f202b01a67a49f5642e
Facu bobarini <facubobarini@gmail.com>
2024-11-05 22:52:21 -0300

    rename CreatePerson to PersonInterface and add new interface: ResponsePersonInterface

--- src/person-module/interface/person.interface.ts
+++ src/person-module/interface/person.interface.ts
@@ -1,6 +1,14 @@
 
-export interface createPerson{
+export interface PersonInterface{
+  _id:string;
   name:string;
   lastName:string;
   dni:number;
+}
+
+export interface ResponsePersonInterface{
+  status:string;
+  message?:any;
+  person?:PersonInterface;
+  people?:PersonInterface[];
 }
